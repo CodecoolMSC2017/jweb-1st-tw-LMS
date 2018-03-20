4824f45168b9d43d4e5d3c4a606ea1cdff0f3b6f
prokajkristof <kiscoloshun@gmail.com>
2018-03-20 11:17:49 +0100

    implemented Mentor class with the getters

--- src/main/java/com/codecool/web/model/Mentor.java
+++ src/main/java/com/codecool/web/model/Mentor.java
@@ -1,4 +1,10 @@
 package com.codecool.web.model;
 
-public class Mentor {
+public class Mentor extends User {
+
+    Mentor(int id, String name, String email, String password, boolean permission) {
+
+        super(id, name, email, password, permission);
+
+    }
 }
