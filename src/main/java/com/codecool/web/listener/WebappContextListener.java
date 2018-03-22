1ee8fe5accecdd7d01f5de3ff792d946391e29a5
Gergely Murvai <murvai.gergely@gmail.com>
2018-03-22 09:10:52 +0100

    WebappContextListener's contextInitialized method refactored ==> *Impl classes

--- src/main/java/com/codecool/web/listener/WebappContextListener.java
+++ src/main/java/com/codecool/web/listener/WebappContextListener.java
@@ -1,8 +1,8 @@
 package com.codecool.web.listener;
 
-import com.codecool.web.service.CourseService;
+import com.codecool.web.service.CourseServiceImpl;
 import com.codecool.web.service.DataContainer;
-import com.codecool.web.service.UserService;
+import com.codecool.web.service.UserServiceImpl;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -15,13 +15,13 @@ public final class WebappContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext cntxt = sce.getServletContext();
-        DataContainer dataContainer = new DataContainer();
-        UserService userService = new UserService();
-        CourseService courseService = new CourseService();
+        DataContainer dataContainer = DataContainer.getInstance();
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        CourseServiceImpl courseServiceImpl = new CourseServiceImpl();
 
         cntxt.setAttribute("dataContainer", dataContainer);
-        cntxt.setAttribute("userService",userService);
-        cntxt.setAttribute("courseService",courseService);
+        cntxt.setAttribute("userServiceImpl", userServiceImpl);
+        cntxt.setAttribute("courseServiceImpl", courseServiceImpl);
 
     }
 
