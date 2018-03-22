b9ca2eee1c368df556d5fc727c6128431966fb18
roskogabor <roskogabor@gmail.com>
2018-03-22 15:37:41 +0100

    addnewcourseservlet

---
+++ src/main/java/com/codecool/web/servlet/NewCourseServlet.java
@@ -0,0 +1,28 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.service.CourseServiceImpl;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/newcourse")
+public class NewCourseServlet  extends HttpServlet {
+    public boolean checkParams(HttpServletRequest req) {
+        return req.getParameter("title") !=null && req.getParameter("desciption") !=null &&
+                !req.getParameter("description").equals("") && !req.getParameter("title").equals("");
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext scx = req.getServletContext();
+        CourseServiceImpl courseServiceImpl = (CourseServiceImpl)scx.getAttribute("courseServiceImpl");
+        if (checkParams(req)) {
+            courseServiceImpl.addNewCourse(req.getParameter("title"), req.getParameter("desciption"));
+            req.getRequestDispatcher("courses.jsp").forward(req, resp);
+        }
+    }
+}
