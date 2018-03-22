12e2b31ee346b3f339294257445274175c7f13a6
Gergely Murvai <murvai.gergely@gmail.com>
2018-03-22 14:04:27 +0100

    Addet isActive Boolean with setter

--- src/main/java/com/codecool/web/model/Course.java
+++ src/main/java/com/codecool/web/model/Course.java
@@ -8,12 +8,14 @@ public class Course {
     private int id;
     private String name;
     private String desc;
+    boolean isActive;
     private List<Task> tasks;
 
     public Course(String name, String desc) {
         this.id = new Random().nextInt(100000);
         this.name = name;
         this.desc = desc;
+        this.isActive = false;
         this.tasks = new ArrayList<>();
     }
 
@@ -31,14 +33,23 @@ public class Course {
         tasks.add(task);
     }
 
-
-
     public void deleteTask(String title) {
         for(Task task : tasks) {
             if(task.getTitle().equals(title)) {
                 tasks.remove(task);
             }
         }
+    }
+
+    public boolean getActive() {
+        return isActive;
+    }
+
+    public void setActivity() {
+        if(getActive()) {
+            isActive = false;
+        }
+        isActive = true;
     }
 
 }
