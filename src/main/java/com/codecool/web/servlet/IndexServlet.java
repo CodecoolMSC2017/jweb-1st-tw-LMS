b7e0037fc230c73eda9d85e4eb74333d717dec1f
Kontra László <kontravlaszlo@gmail.com>
2018-04-03 13:23:01 +0200

    Deleted home.jsp and refactored accordingly

--- src/main/java/com/codecool/web/servlet/IndexServlet.java
+++ src/main/java/com/codecool/web/servlet/IndexServlet.java
@@ -1,16 +1,22 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.model.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/home")
 public class IndexServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("home.jsp").forward(req, resp);
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
+        req.setAttribute("user", user);
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
     }
 }
