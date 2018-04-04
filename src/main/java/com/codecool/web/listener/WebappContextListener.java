c138c893a838fd2cffc29b2f23c83a1e464fcbfc
Gergely Murvai <murvai.gergely@gmail.com>
2018-04-04 07:12:54 +0200

    Cleaned up unused imports.

--- src/main/java/com/codecool/web/listener/WebappContextListener.java
+++ src/main/java/com/codecool/web/listener/WebappContextListener.java
@@ -2,7 +2,6 @@ package com.codecool.web.listener;
 
 import com.codecool.web.service.CourseServiceImpl;
 import com.codecool.web.service.DataContainer;
-import com.codecool.web.service.UserServiceImpl;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -14,13 +13,12 @@ public final class WebappContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+
         ServletContext cntxt = sce.getServletContext();
         DataContainer dataContainer = DataContainer.getInstance();
-        //UserServiceImpl userServiceImpl = new UserServiceImpl();
         CourseServiceImpl courseServiceImpl = new CourseServiceImpl();
 
         cntxt.setAttribute("dataContainer", dataContainer);
-        //cntxt.setAttribute("userServiceImpl", userServiceImpl);
         cntxt.setAttribute("courseServiceImpl", courseServiceImpl);
 
     }
