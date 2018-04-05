881a9daea7f9f0180eb48a23cd90de711d63a495
Gergely Murvai <murvai.gergely@gmail.com>
2018-04-05 15:32:58 +0200

    maxPoint,pointsGit fields and their getters and setters created

--- src/main/java/com/codecool/web/model/Assignment.java
+++ src/main/java/com/codecool/web/model/Assignment.java
@@ -3,13 +3,17 @@ package com.codecool.web.model;
 public class Assignment extends Course {
 
     private String submission;
+    private int maxPoint;
+    private int pointsGot;
 
     public Assignment(String name, String desc) {
         super(name, desc);
+        this.maxPoint = 10;
+        this.pointsGot = 0;
     }
 
-    public Assignment(int id,String name, String desc, boolean isActive,int assignId , String submission) {
-        super(id,name, desc,isActive);
+    public Assignment(int id, String name, String desc, boolean isActive, int assignId, String submission) {
+        super(id, name, desc, isActive);
         this.submission = submission;
     }
 
@@ -18,13 +22,32 @@ public class Assignment extends Course {
         return submission;
     }
 
-    /*public void setSubmission() {
-
-    }*/
-
+    public void setSubmission(String submission) {
+        this.submission = submission;
+    }
 
 
     public boolean isActive() {
         return isActive;
+    }
+
+    public int getMaxPoint() {
+        return maxPoint;
+    }
+
+    public void setMaxPoint(int maxPoint) {
+        this.maxPoint = maxPoint;
+    }
+
+    public int getPointsGot() {
+        return pointsGot;
+    }
+
+    public void setPointsGot(int points) {
+        if (maxPoint < points) {
+            pointsGot = maxPoint;
+        } else {
+            pointsGot = points;
+        }
     }
 }
