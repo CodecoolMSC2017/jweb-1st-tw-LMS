3a11106b6442460e1b6e559ed86307903eec49f3
Kontra László <kontravlaszlo@gmail.com>
2018-04-17 14:26:59 +0200

    implemented search, register and edit features for user

--- src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
@@ -1,10 +1,8 @@
 package com.codecool.web.dao.database;
 
-
-
-
 import com.codecool.web.dao.UserDao;
 import com.codecool.web.model.User;
+import com.codecool.web.service.RegisterServiceImpl;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -17,7 +15,7 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
     }
 
     public List<User> findAll() throws SQLException {
-        String sql = "SELECT id, email, password FROM users";
+        String sql = "SELECT * FROM users";
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
             List<User> users = new ArrayList<>();
@@ -33,7 +31,7 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
         if (email == null || "".equals(email)) {
             throw new IllegalArgumentException("Email cannot be null or empty");
         }
-        String sql = "SELECT id, email, password FROM users WHERE email = ?";
+        String sql = "SELECT id, name, email, password FROM users WHERE email = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, email);
             try (ResultSet resultSet = statement.executeQuery()) {
@@ -45,10 +43,87 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
         return null;
     }
 
+    public User findByName(String name) throws SQLException {
+        if (name == null || "".equals(name)) {
+            throw new IllegalArgumentException("name cannot be null or empty");
+        }
+        String sql = "SELECT id, name, email, password FROM users WHERE name = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, name);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return fetchUser(resultSet);
+                }
+            }
+        }
+        return null;
+    }
+
+    public User findByid(int id) throws SQLException {
+        String sql = "SELECT id, name, email, password FROM users WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return fetchUser(resultSet);
+                }
+            }
+        }
+        return null;
+    }
+
+    public User registerUser (String name, String email, String password, boolean permission) throws SQLException{
+        if (name == null || "".equals(name) || email == null || "".equals(email) || password == null || "".equals(password)) {
+            throw new IllegalArgumentException("Fields can not be empty");
+        }
+        boolean autoCommit = connection.getAutoCommit();
+        connection.setAutoCommit(false);
+        String sql = "INSERT INTO users (name, email, password, permission) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
+            statement.setString(1, name);
+            statement.setString(2, email);
+            statement.setString(3, password);
+            statement.setBoolean(4, permission);
+            executeInsert(statement);
+            int id = fetchGeneratedId(statement);
+            connection.commit();
+            return new User(id, name, email, password, permission);
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(autoCommit);
+        }
+    }
+
+    public void editUser(String email, String password, int id) throws SQLException {
+        if (email == null || "".equals(email) || password == null || "".equals(password)) {
+            throw new IllegalArgumentException("Fields can not be empty");
+        }
+        boolean autoCommit = connection.getAutoCommit();
+        connection.setAutoCommit(false);
+        String sql = "UPDATE users SET email = ?, password = ? WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
+            statement.setString(1, email);
+            statement.setString(2, password);
+            statement.setInt(3, id);
+            executeInsert(statement);
+            connection.commit();
+
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(autoCommit);
+        }
+    }
+
     private User fetchUser(ResultSet resultSet) throws SQLException {
         int id = resultSet.getInt("id");
+        String name = resultSet.getString("name");
         String email = resultSet.getString("email");
         String password = resultSet.getString("password");
-        return new User(id, email, password);
+        Boolean permission = resultSet.getBoolean("permission");
+        return new User(id, name, email, password, permission);
     }
 }
