9216df3fd162c2e68c14a09df4a7ac7a9c3568f8
prokajkristof <kiscoloshun@gmail.com>
2018-04-17 14:46:44 +0200

    implemented CourseDao

--- src/main/java/com/codecool/web/dao/CourseDao.java
+++ src/main/java/com/codecool/web/dao/CourseDao.java
@@ -1,5 +1,24 @@
 package com.codecool.web.dao;
 
-public interface CourseDao {
+import com.codecool.web.model.Course;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+interface CourseDao {
+    List<Course> findAll() throws SQLException ;
+
+    Course fetchCourse(ResultSet resultSet) throws SQLException ;
+
+    Course findById(int id) throws SQLException ;
+
+    void add(String name, String description, boolean isActive) throws SQLException ;
+
+    void remove(int id) throws SQLException ;
+
+    void editCourse(int id, String name, String description, boolean isActive) throws SQLException ;
 }
